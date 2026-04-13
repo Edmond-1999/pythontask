c5de1d97e5caa9609a19aecb6e3770422900a03b
Edmond-1999 <agburugaedmond@gmail.com>
2026-04-13 10:48:10 +0100

    chaptertwo

--- Chapter2/2_2.py
+++ Chapter2/2_2.py
@@ -1,2 +1,2 @@
-userRating = int(input("Enter an integer rating between 1 and 10: "))
-print("You entered:", userRating)
+user_rating = int(input("Enter an integer rating between 1 and 10: "))
+print("You entered:", user_rating)
