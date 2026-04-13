65e933c494d09ee5c1c30f0a363ede6fb0c58a4e
Edmond-1999 <agburugaedmond@gmail.com>
2026-04-13 10:53:14 +0100

    chaptertwo

--- Chapter2/2_7.py
+++ Chapter2/2_7.py
@@ -1,7 +1,7 @@
-inputNumber = int(input("Enter an integer: "))
+input_number = int(input("Enter an integer: "))
 
-if inputNumber % 4 == 0:
+if input_number % 4 == 0:
     print("The number is a multiple of 4")
 
-if inputNumber % 2 == 0:
+if input_number % 2 == 0:
     print("The number is a multiple of 2")
