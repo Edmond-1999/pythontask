301c174dd5cd9c51a960dba1ac8fef3b44d142ca
Edmond-1999 <agburugaedmond@gmail.com>
2026-04-13 10:45:01 +0100

    chaptertwo

--- Chapter2/2_1.py
+++ Chapter2/2_1.py
@@ -3,5 +3,5 @@ second_number = 3
 
 print("first_number =", first_number)
 print("Value of first_number + second_number is", first_number + second_number)
-print("first_number =")
+print("first_number = ")
 print(first_number + second_number, "=", second_number + first_number)
