0a8eb806a31e054e35273f4163532c3f627716af
Edmond-1999 <agburugaedmond@gmail.com>
2026-04-13 10:36:46 +0100

    Update variable names to snake_case format in 2_1.py

--- Chapter2/2_1.py
+++ Chapter2/2_1.py
@@ -1,7 +1,7 @@
 first_number = 2
 second_number = 3
 
-print("firstNumber =", first_number)
-print("Value of firstNumber + secondNumber is", first_number + second_number)
-print("firstNumber =")
+print("first_number =", first_number)
+print("Value of first_number + second_number is", first_number + second_number)
+print("first_number =")
 print(first_number + second_number, "=", second_number + first_number)
