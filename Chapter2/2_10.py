5a398bdd95de515136bd6afe42089f9f11765aba
Edmond-1999 <agburugaedmond@gmail.com>
2026-04-13 10:47:25 +0100

    chaptertwo

--- Chapter2/2_10.py
+++ Chapter2/2_10.py
@@ -2,16 +2,16 @@ number1 = int(input("Enter first number: "))
 number2 = int(input("Enter second number: "))
 number3 = int(input("Enter third number: "))
 
-totalSum = number1 + number2 + number3
-averageValue = totalSum / 3
+total_sum = number1 + number2 + number3
+average_value = totalSum / 3
 
-productValue = number1 * number2 * number3
+product_value = number1 * number2 * number3
 
-smallestValue = min(number1, number2, number3)
-largestValue = max(number1, number2, number3)
+smallest_value = min(number1, number2, number3)
+largest_value = max(number1, number2, number3)
 
-print("Sum:", totalSum)
-print("Average:", averageValue)
-print("Product:", productValue)
-print("Smallest:", smallestValue)
-print("Largest:", largestValue)
+print("Sum:", total_sum)
+print("Average:", average_value)
+print("Product:", product_value)
+print("Smallest:", smallest_value)
+print("Largest:", largest_value)
